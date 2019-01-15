1fde34b521df7d21701d349eaed4b833294ac5da
Ozan <38402864+oozayranci@users.noreply.github.com>
2019-01-15 23:48:04 +0200

    Add files via upload

---
+++ build/precache-manifest.09f196ac8a1780eb3564aab3dbb8c3bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ed9dc1603f66a5ded984",
+    "url": "/static/js/main.ed9dc160.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "ed9dc1603f66a5ded984",
+    "url": "/static/css/main.f22d088e.chunk.css"
+  },
+  {
+    "revision": "b93a7ef6eb567966368127dd348cf4fe",
+    "url": "/index.html"
+  }
+];
